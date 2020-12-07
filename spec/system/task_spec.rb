e8244708cc598fc4c33f25c39b1f74513ec8d745
krpk1900 <krpk1900@gmail.com>
2020-12-07 10:23:05 +0900

    [Fix] ローカル変数をletに変更

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
   describe 'Task一覧' do
+    let(:project){ create(:project) }
+    let(:task){ create(:task, project_id: project.id) }
     context '正常系' do
       it '一覧ページにアクセスした場合、Taskが表示されること' do
-        # TODO: ローカル変数ではなく let を使用してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
+        # DONE: ローカル変数ではなく let を使用してください
         visit project_tasks_path(project)
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
@@ -15,10 +15,10 @@ RSpec.describe 'Task', type: :system do
 
       xit 'Project詳細からTask一覧ページにアクセスした場合、Taskが表示されること' do
         # FIXME: テストが失敗するので修正してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
         visit project_path(project)
+          puts project.id
         click_link 'View Todos'
+          puts task.project_id
         expect(page).to have_content task.title
         expect(Task.count).to eq 1
         expect(current_path).to eq project_tasks_path(project)
@@ -27,10 +27,10 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task新規作成' do
+    let(:project){ create(:project) }
     context '正常系' do
       it 'Taskが新規作成されること' do
-        # TODO: ローカル変数ではなく let を使用してください
-        project = FactoryBot.create(:project)
+        # DONE: ローカル変数ではなく let を使用してください
         visit project_tasks_path(project)
         click_link 'New Task'
         fill_in 'Title', with: 'test'
@@ -43,11 +43,11 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task詳細' do
+    let(:project){ create(:project) }
+    let(:task){ create(:task, project_id: project.id) }
     context '正常系' do
       it 'Taskが表示されること' do
-        # TODO: ローカル変数ではなく let を使用してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
+        # DONE: ローカル変数ではなく let を使用してください
         visit project_task_path(project, task)
         expect(page).to have_content(task.title)
         expect(page).to have_content(task.status)
@@ -58,6 +58,8 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe 'Task編集' do
+    let(:project){ create(:project) }
+    let(:task){ create(:task, project_id: project.id) }
     context '正常系' do
       xit 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
@@ -72,9 +74,7 @@ RSpec.describe 'Task', type: :system do
       end
 
       it 'ステータスを完了にした場合、Taskの完了日に今日の日付が登録されること' do
-        # TODO: ローカル変数ではなく let を使用してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
+        # DONE: ローカル変数ではなく let を使用してください
         visit edit_project_task_path(project, task)
         select 'done', from: 'Status'
         click_button 'Update Task'
